7a23b9f807e54f82b11163c95d86782a06847494
Kevin Wyckmans <kevinwyckmans@gmail.com>
2021-01-30 20:55:47 +0100

    wire up buttons to move through all scenes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-        public void LoadNextScene(){
+    public void LoadNextScene(){
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+
+    public void LoadStartScene(){
+        SceneManager.LoadScene(0);
+    }
 }
