2142624873dcab76d09119ffd5cedc2ac8132b95
Kevin Wyckmans <kevinwyckmans@gmail.com>
2021-02-05 21:19:23 +0100

    generate a random nunber between a range as a guess

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    List<int> guesses = new List<int>();
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        if(this.min >= this.max){
+            Debug.LogError("Min value is larger than max value");
+        }
+
+        this.NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        this.min = this.guess + 1;
+        Debug.Log("Need to guess higher, new max is " + this.min.ToString());
+
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        this.max = this.guess - 1;
+        Debug.Log("Need to guess lower, new max is " + this.max.ToString());
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        this.guess = Random.Range(this.min, this.max + 1);
+        guessText.SetText(guess.ToString());
+    }
+}
